b8527d23df575ed0b6881be1b789c8b19b14ab0b
Caio Martins <caio_martins02@yahoo.com.br>
2022-02-12 00:27:53 -0300

    i18n example

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Localization from "expo-localization";
+import i18n from "i18n-js";
 
+i18n.locale = Localization.locale;
+i18n.fallbacks = true;
+
+i18n.translations = {
+  en: {
+    "hello world": "Hello World!",
+    welcome: "Open up App.js to start working on your app!",
+  },
+  pt: {
+    "hello world": "Olá mundo!",
+    welcome: "Abra o App.js para começar a trabalhar no seu aplicativo!",
+  },
+};
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{i18n.t("hello world")}</Text>
+      <Text>{i18n.t("welcome")}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
